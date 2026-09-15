ae2ebeaf0d3f43bf21135dcfa9f96e0f7f565559
Regan Bell <regan@ycombinator.com>
2026-09-14 19:28:12 -0700

    Preserve the HTTP idle timeout explicitly

--- cli/templates/aws/main.tf
+++ cli/templates/aws/main.tf
@@ -993,6 +993,7 @@ resource "aws_ecs_service" "service" {
       port_name      = each.key
       discovery_name = each.key
       timeout {
+        idle_timeout_seconds        = 300
         per_request_timeout_seconds = 0
       }
       client_alias {
